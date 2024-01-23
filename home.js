f8e1d8af2f228548cfce59ed60430ebae0a4d3fb
walidgzih12 <walidgzih3@gmail.com>
2024-01-23 03:05:05 +0100

    done

--- home.js
+++ home.js
@@ -12,7 +12,8 @@ var movies = [
         description: 'the dark knight streaming online, the dark knight movie stream, the dark knight online movie download, watch the dark knight online, the dark knight watch online, the dark knight free download',
         date: 'Jul 18,2008',
         pic: 'https://images-na.ssl-images-amazon.com/images/S/pv-target-images/57618417a0ddf3b7aa29db658bef2fc3b9608bb11a021631d3b6408441c1e0f7._RI_TTW_.jpg'
-    },    {
+    },  
+      {
         name: 'Brooklyn Knight',
         duration: '94 min',
         type: 'Movie',
@@ -23,7 +24,8 @@ var movies = [
         description: 'When Two rival basketball coaches played as Kirk "Sticky Fingaz" Jones and Khalil Kain fall out over a bad call in a championship game, things only heat up and get steamy and treacherous when Knight Mathews hooks up with the lovel...',
         date: 'Mar 22, 2022',
         pic: 'https://m.media-amazon.com/images/M/MV5BY2Q2YWUyOWYtMzBiYS00MTQ0LTkwOGQtYjljY2EzOTU0YWE0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNzA3NDAwMjc@._V1_.jpg'
-    },    {
+    },  
+      {
         name: 'Breaking Bad',
         duration: '',
         type: 'TV',
@@ -34,7 +36,8 @@ var movies = [
         description: 'A chemistry teacher diagnosed with inoperable lung cancer turns to manufacturing and selling methamphetamine with a former student in order to secure his familys future',
         date: 'Jan 20, 2008',
         pic: 'https://fr.web.img3.acsta.net/c_310_420/pictures/19/06/18/12/11/3956503.jpg'
-    },    {
+    },  
+      {
         name: 'The Blacklist',
         duration: '',
         type: 'Serie',
@@ -45,7 +48,8 @@ var movies = [
         description: 'A new FBI profiler, Elizabeth Keen, has her entire life uprooted when a mysterious criminal, Raymond Reddington, who has eluded capture for decades, turns himself in and insists on speaking only to her',
         date: 'Sep 23, 2013',
         pic: 'https://m.media-amazon.com/images/M/MV5BMTU1OTdjYTUtMzA2MS00Njg4LWI1NTctMWUzYzNkNmQ5YWY3XkEyXkFqcGdeQXVyMTUwMzM5ODkz._V1_FMjpg_UX1000_.jpg'
-    },    {
+    },   
+     {
         name: 'Lupin',
         duration: '',
         type: 'Serie',
@@ -56,7 +60,8 @@ var movies = [
         description: 'Inspired by the adventures of Arsène Lupin, gentleman thief Assane Diop sets out to avenge his father for an injustice inflicted by a wealthy family',
         date: 'Jan 08, 2021',
         pic: 'https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p19204904_b_v13_al.jpg'
-    },    {
+    },  
+      {
         name: 'The GodFather',
         duration: '175 min',
         type: 'Movie',
@@ -67,7 +72,8 @@ var movies = [
         description: 'Don Vito Corleone, head of a mafia family, decides to hand over his empire to his youngest son Michael. However, his decision unintentionally puts the lives of his loved ones in grave danger.',
         date: 'Mar 24, 1972',
         pic: 'https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg'
-    },    {
+    },   
+     {
         name: 'Lift',
         duration: '107 min',
         type: 'Movie',
@@ -79,6 +85,18 @@ var movies = [
         date: 'Jan 12, 2024',
         pic: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/Lift_2024_poster.webp/259px-Lift_2024_poster.webp.png'
     },
+    {
+        name: 'Napoleon',
+        duration: '158 min', 
+        type: 'Movie',
+        genre: 'Adventure, Action, Biography',
+        director: 'Ridley Scott',
+        production: 'Apple Studios, Scott Free Productions, Moving Picture Company (MPC)',
+        country: 'United States',
+        description: 'An epic that details the checkered rise and fall of French Emperor Napoleon Bonaparte and his relentless journey to power through the prism of his addictive, volatile relationship with his wife, Josephine.',
+        date: 'Nov 22, 2023',
+        pic: 'https://static.bunnycdn.ru/i/cache/images/9/96/96e23d7dcf54ec4f1b54edf5a57b5486.jpg'
+    }
 
 ];
 
@@ -88,13 +106,22 @@ function createMovieCards(array, searchName) {
 
     array.forEach(function(movie) {
         if (!searchName || movie.name.toLowerCase().includes(searchName.toLowerCase())) {
-            var movieCard = document.createElement('div');
-            movieCard.classList.add('movie-card');
 
+            var movieCard = document.createElement('div');
+            movieCard.classList.add('movie-card'); 
+
+
+            
+    function createMovieDetail(className, textContent) {
+    var detail = document.createElement('div');
+    detail.classList.add(className);
+    detail.textContent = textContent;
+    return detail;
+}
             
             var poster = document.createElement('img');
             poster.src = movie.pic;
-            poster.alt = movie.name + ' Poster';
+            poster.alt = movie.name + 'Poster';
             poster.classList.add('movie-poster');
 
             var title = createMovieDetail('movie-title', movie.name);
@@ -119,19 +146,13 @@ function createMovieCards(array, searchName) {
             movieCard.appendChild(description);
             movieCard.appendChild(date);
 
-            
             container.appendChild(movieCard);
         }
     });
 }
 
 
-function createMovieDetail(className, textContent) {
-    var detail = document.createElement('div');
-    detail.classList.add(className);
-    detail.textContent = textContent;
-    return detail;
-}
+
 
 
 var searchBar = document.querySelector('.search-bar input');
